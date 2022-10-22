21b3ad69377adf803ff7712a45fefd83c7582bc0
christianrhodes18 <thechristianrhodes@gmail.com>
2022-10-22 17:03:38 -0400

    added ProfileView.swift

---
+++ gift-tracker/Views/ProfileView.swift
@@ -0,0 +1,39 @@
+//
+//  ProfileView.swift
+//  gift-tracker
+//
+//  Created by Christian Rhodes on 10/22/22.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        VStack {
+            ProfilePicture()
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("John Appleseed")
+                        .font(.title)
+                    Spacer()
+                    Text("July 4th")
+                        .font(.subheadline)
+                }
+                
+                Divider()
+                
+                Text("John's birthday is in 100 days.")
+                    .font(.title2)
+                Text("Description...")
+                    .padding(5)
+            }
+            .padding()
+        }
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
