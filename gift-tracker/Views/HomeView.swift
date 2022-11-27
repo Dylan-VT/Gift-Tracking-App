5851a0fbd305cd0a9231101ebf75e13b5bd57884
elochar <cf.hartnett12@gmail.com>
2022-11-26 19:14:35 -0500

    added some of the handling for pulling user data from the api to display on the home page. For now it doesn't work so we are still using dummy data

--- gift-tracker/Views/HomeView.swift
+++ gift-tracker/Views/HomeView.swift
@@ -11,45 +11,47 @@ let dummyFriends: [UserAccount]  = [UserAccount(birthday: "2001-11-17", display_
 
 struct HomeView: View {
     //create an instance of the ViewModel
-    @StateObject var viewModel = ViewModel()
-    //---------------------------
-    //@State var profiles: [UserAccount]
+    //@StateObject var viewModel = ViewModel()
+    
+    @State var profiles: [UserAccount]
     
     var body: some View {
         NavigationView {
             List{
-               // ForEach(profiles) {profile in
-                 //   NavigationLink(profile.display_name, destination: PrivateProfileView(user:profile))
-                //}
-                //--------------------------- new loop for the users names
-                ForEach(viewModel.users, id: \.self) { user in
-                    //create a stack for the users
-                    HStack {
-                        Text(user.name)
-                            .bold()
-                        Text(user.bday)
-                            .bold()
-                    }
-                    .padding(3)
+                ForEach(profiles) {profile in
+                    NavigationLink(profile.display_name, destination: PrivateProfileView(user:profile))
+                    //}
+                    //new loop for the users names
+                    /*
+                     ForEach(viewModel.users, id: \.self) { user in
+                     //create a stack for the users
+                     HStack {
+                     Text(user.name)
+                     .bold()
+                     Text(user.bday)
+                     .bold()
+                     }
+                     .padding(3)
+                     } */
                 }
-            }
-            .navigationTitle("Upcoming Birthdays")
-            //---------------------------
-            .onAppear {
-                viewModel.fetchUser()
+                .navigationTitle("Upcoming Birthdays")
+                //---------------------------
+                //.onAppear {
+                //  viewModel.fetchUser()
+                //}
             }
         }
+        //---------------------------
     }
-    //---------------------------
+    
+    
+    /*
+     
+     struct HomeView_Previews: PreviewProvider {
+     @State var friends: [UserAccount] = dummyFriends
+     static var previews: some View {
+     HomeView(profiles: $friends)
+     }
+     }
+     */
 }
-
-
-/*
-
-struct HomeView_Previews: PreviewProvider {
-    @State var friends: [UserAccount] = dummyFriends
-    static var previews: some View {
-        HomeView(profiles: $friends)
-    }
-}
-*/
--- gift-tracker/Views/listView.swift
+++ gift-tracker/Views/listView.swift
@@ -19,13 +19,14 @@ class ViewModel: ObservableObject {
     
     func fetchUser() {
         //create url object and unwarp with guard let NOTE: For now it is hardcodded in
-        guard let url = URL(string: "http://54.237.192.235/getuser/someUser1231") else { return }
+        guard let url = URL(string: "http://54.237.192.235/getallusers") else { return }
         //perform apicall
         let task = URLSession.shared.dataTask(with: url) { [weak self]data, _, error in guard let data = data, error == nil else { return }
         //conver to json
         do {
             //try json decoder
             let users = try JSONDecoder().decode([UserROW].self, from: data)
+            print(users)
             //reminnd the view to update published proterty
             DispatchQueue.main.async {
                 self?.users = users
