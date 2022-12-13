395604a60000b95070cb59fdad5311cf89953e50
christianrhodes18 <thechristianrhodes@gmail.com>
2022-12-12 19:55:47 -0500

    private profile styling

--- gift-tracker/Views/PrivateProfileView.swift
+++ gift-tracker/Views/PrivateProfileView.swift
@@ -13,40 +13,62 @@ struct PrivateProfileView: View {
     @Binding var user: UserAccount
     @Binding var friendsList: String
     @Binding var friendEvents: [FriendEvent]
+    
     var body: some View {
-        NavigationView{
-            VStack {
-                ProfilePicture(image: Image("profile_picture_1"))
-                VStack(alignment: .leading) {
-                    HStack {
-                        Text(user.display_name)
-                            .font(.title)
-                        Spacer()
-                        Text(formatBirthday(user.birthday))
-                            .font(.subheadline)
+        ZStack {
+            Color.myBeige
+                .ignoresSafeArea()
+            
+            NavigationView{
+                VStack {
+                    ProfilePicture(image: Image("profile_picture_1"))
+                        .offset(x: -80, y: -110)
+                        //.padding(.bottom, -50)
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text(user.display_name)
+                                .font(.title)
+                                .colorInvert()
+                            Spacer()
+                            Text(formatBirthday(user.birthday))
+                                .font(.subheadline)
+                                .colorInvert()
+                        }
+                        
+                        Divider()
+                            .colorInvert()
+                        
+                        let daysToB = daysToBirthday(user.birthday)
+                        
+                        if daysToB == 0{
+                            Text("Your birthday is Today!")
+                                .font(.title2)
+                                .colorInvert()
+                        }
+                        else{
+                            Text("Your birthday is in \(daysToB) days.")
+                                .font(.title2)
+                                .colorInvert()
+                        }
                     }
-                    
-                    Divider()
-                    let daysToB = daysToBirthday(user.birthday)
-                    
-                    if daysToB == 0{
-                        Text("Your birthday is Today!")
-                            .font(.title2)
+                    .offset(y: -110)
+                    .padding()
+                    NavigationLink(destination: AddFriendView(user: $user, friendsList: $friendsList, friendEvents: $friendEvents)){
+                        Text("Add Friends")
                     }
-                    else{
-                        Text("Your birthday is in \(daysToB) days.")
-                            .font(.title2)
-                    }
+                    .padding(.vertical)
+                    .padding(.horizontal, 100)
+                    .foregroundColor(Color.white)
+                    .background(Color.myLightGreen)
+                    .clipShape(Capsule())
+                    .offset(y: -50)
                 }
-                .padding()
-                NavigationLink(destination: AddFriendView(user: $user, friendsList: $friendsList, friendEvents: $friendEvents)){
-                    Text("Add Friends")
-                }
-                .padding(.vertical)
-                .padding(.horizontal, 100)
-                .foregroundColor(Color.white)
-                .background(Color.blue)
-                .clipShape(Capsule())
+                .frame(width: UIScreen.screenWidth, height: 700)
+                .offset(y: 10)
+                //.offset(y: -150) use this for the private profile
+                .background(Color.myDarkGreen)
+                .offset(y: -50)
+                Spacer()
             }
         }
     }
